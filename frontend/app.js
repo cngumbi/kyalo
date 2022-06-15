ece718d988d8b749e03e2c8b6af8fc4913d13edf
cngumbi <cngumbi35@gmail.com>
2022-06-15 14:56:52 +0300

    added new files

---
+++ frontend/app.js
@@ -0,0 +1,6 @@
+import HomeScreen from "./screens/homeScreen.js";
+const router = () => {
+    const main = document.getElementById("main-container");
+    main.innerHTML = homeScreen.render();
+};
+window.addEventListener('load', router);
---
+++ frontend/data.js
@@ -0,0 +1,71 @@
+export default {
+    products: [{
+            _id: '1',
+            name: 'Shirt official',
+            category: 'Shirts',
+            image: 'style/images/1.png',
+            price: 100,
+            brand: 'official',
+            rating: 4.5,
+            numReviews: 20,
+            countInStock: 10
+        },
+        {
+            _id: '2',
+            name: 'Shirt official',
+            category: 'Shirts',
+            image: 'style/images/1.png',
+            price: 100,
+            brand: 'official',
+            rating: 4.5,
+            numReviews: 20,
+            countInStock: 10
+        },
+        {
+            _id: '3',
+            name: 'Shirt official',
+            category: 'Shirts',
+            image: 'style/images/1.png',
+            price: 100,
+            brand: 'official',
+            rating: 4.5,
+            numReviews: 20,
+            countInStock: 10
+        },
+        {
+            _id: '4',
+            name: 'Shirt official',
+            category: 'Shirts',
+            image: 'style/images/1.png',
+            price: 100,
+            brand: 'official',
+            rating: 4.5,
+            numReviews: 20,
+            countInStock: 10
+        },
+        {
+            _id: '5',
+            name: 'Shirt official',
+            category: 'Shirts',
+            image: 'style/images/1.png',
+            price: 100,
+            brand: 'official',
+            rating: 4.5,
+            numReviews: 20,
+            countInStock: 10
+        },
+        {
+            _id: '6',
+            name: 'Shirt official',
+            category: 'Shirts',
+            image: 'style/images/1.png',
+            price: 100,
+            brand: 'official',
+            rating: 4.5,
+            numReviews: 20,
+            countInStock: 10
+        },
+
+
+    ],
+};
