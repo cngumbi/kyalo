d09e071de0ae35614c866fac635c6bf2e12aacb5
cngumbi <cngumbi35@gmail.com>
2022-06-16 20:06:27 +0300

    renamed file homeScreen.js to mainSection.js

---
+++ frontend/src/screens/homePage/mainSection.js
@@ -0,0 +1,29 @@
+import data from "../../data.js";
+const MainSection = {
+        render: () => {
+                const { products } = data;
+                return `
+                <ul class="card">
+                    ${products.map( product =>`
+                    <li>
+                        <div class="card-item">
+                            <a href="#/product/${product._id}">
+                                <img src="${product.image}" alt="${product.name}">
+                            </a>
+                            <div class="item-name">
+                                <a href="#/product/1">${product.name}</a>
+                            </div>
+                            <div class="item-brand">
+                                ${product.brand}
+                            </div>
+                            <div class="item-price">
+                            ${product.price}
+                            </div>
+
+                        </div>
+                    </li>
+                    `).join('\n')}
+                `;
+    },
+};
+export default MainSection;
