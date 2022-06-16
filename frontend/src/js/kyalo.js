8fc815b0d433ec1b919277aef493a17fb96ad3b8
cngumbi <cngumbi35@gmail.com>
2022-06-16 23:23:14 +0300

    modified file index.html to add the script to of kyalo.js and main.js and modified by creating a javascript library

---
+++ frontend/src/js/kyalo.js
@@ -0,0 +1,8 @@
+//creating the library
+//author: Softcraze Corporation
+//date: June sixteenth Two Thuosand Twenty-Two 
+function $$(selector) {
+    const self = {
+        element: document.querySelector(selector),
+    }
+}
