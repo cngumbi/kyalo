7f36ce8a6925c223f3b477ae8050d3adbf2a2437
cngumbi <cngumbi35@gmail.com>
2022-06-16 20:05:34 +0300

    modified files app.js and index.html

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,6 @@
-import HomeScreen from "./screens/homeScreen.js";
+import MainSection from "./screens/homePage/mainSection.js";
 const router = () => {
     const main = document.getElementById("main-container");
-    main.innerHTML = HomeScreen.render();
+    main.innerHTML = MainSection.render();
 };
 window.addEventListener('load', router);
