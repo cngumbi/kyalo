5ace50a3ffa2ba9a9b4f3d09d22c5b82d81f5c80
cngumbi <cngumbi35@gmail.com>
2022-06-18 11:06:16 +0300

    modified the files to handle more rources

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,21 @@
 import MainSection from "./screens/homePage/mainSection.js";
+import ProductSection from "./screens/productPage/productSection.js";
+import { parseRequestUrl } from "./util.js";
+import Error404Section from "./screens/productPage/error404Section.js";
+//create the route of the web pages
+const routes = {
+    '/': MainSection,
+    '/product/:id': ProductSection,
+
+};
 const router = () => {
+    const request = parseRequestUrl();
+    const parseUrl = (request.resource ? `/${request.resource}` : `/`) +
+        (request.id ? '/:id' : '') +
+        (request.verb ? `/${request.verb}` : '');
+    const section = routes[parseUrl] ? routes[parseUrl] : Error404Section;
     const main = document.getElementById("main-container");
-    main.innerHTML = MainSection.render();
+    main.innerHTML = section.render();
 };
 window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
---
+++ frontend/src/js/main.js
@@ -0,0 +1,10 @@
+//this main section will hold the details of the main pages
+//Author: Softcraze Corporation
+//Date Eighteenth June Two Thousand Twenty-Two
+
+const headerLinks = {
+    login: "#",
+    signup: "#",
+    chats: "#"
+
+};
