89e9afd40fb7cccd57d2ee9e9cad390228182095
cngumbi <cngumbi35@gmail.com>
2022-06-21 23:06:10 +0300

    modified the file to handle the new installed libraries

--- frontend/src/screens/homePage/mainSection.js
+++ frontend/src/screens/homePage/mainSection.js
@@ -1,17 +1,19 @@
 //import data from "../data.js";
+import axios from 'axios';
 const MainSection = {
         render: async() => {
                 //const { products } = data;
 
-                const response = await fetch("http://localhost:5000/api/products", {
+                const response = await axios({
+                    url: 'http://localhost:5000/api/products',
                     headers: {
                         'Content-Type': 'application/json',
                     },
                 });
-                if (!response || !response.ok) {
+                if (!response || response.statusText !== 'OK') {
                     return `<div>Error in getting data</div>`;
                 }
-                const products = await response.json();
+                const products = response.data;
                 return `
                 <ul class="card">
                     ${products.map( product =>`
