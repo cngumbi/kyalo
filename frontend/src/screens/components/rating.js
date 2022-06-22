d8d7c3b0a35cf9f324e3573c60a4ae6231db597d
cngumbi <cngumbi35@gmail.com>
2022-06-22 12:26:12 +0300

    added the rating.js file to handle and calculate rating

---
+++ frontend/src/screens/components/rating.js
@@ -0,0 +1,59 @@
+const Rating = {
+    render: (props) => {
+        if (!props.value) {
+            return '<div></div>';
+        }
+        return `
+            <div class="rating">
+                <span>
+                    <i class="${
+                        props.value >= 1
+                          ? 'sc sc-star'
+                          : props.value >= 0.5
+                          ? 'sc sc-star-half-f'
+                          : 'sc sc-star-e'
+                    }"></i>
+                </span>
+                <span>
+                    <i class="${
+                        props.value >= 2
+                          ? 'sc sc-star'
+                          : props.value >= 1.5
+                          ? 'sc sc-star-half-f'
+                          : 'sc sc-star-e'
+                    }"></i>
+                </span>
+                <span>
+                    <i class="${
+                        props.value >= 3
+                          ? 'sc sc-star'
+                          : props.value >= 2.5
+                          ? 'sc sc-star-half-f'
+                          : 'sc sc-star-e'
+                    }"></i>
+                </span>
+                <span>
+                    <i class="${
+                        props.value >= 4
+                          ? 'sc sc-star'
+                          : props.value >= 3.5
+                          ? 'sc sc-star-half-f'
+                          : 'sc sc-star-e'
+                    }"></i>
+                </span>
+                <span>
+                    <i class="${
+                        props.value >= 5
+                          ? 'sc sc-star'
+                          : props.value >= 4.5
+                          ? 'sc sc-star-half-f'
+                          : 'sc sc-star-e'
+                    }"></i>
+                </span>
+                <span> ${props.text || ''} </span>
+            </div>
+        `;
+    },
+
+};
+export default Rating;
