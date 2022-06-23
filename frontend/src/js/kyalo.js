6b678ff0cb51de44180002660a839492ef42a558
cngumbi <cngumbi35@gmail.com>
2022-06-23 20:20:01 +0300

    modified the files while designing the product page

--- frontend/src/js/kyalo.js
+++ frontend/src/js/kyalo.js
@@ -1,9 +1,33 @@
 //creating the library
 //author: Softcraze Corporation
-//date: June sixteenth Two Thuosand Twenty-Two 
-function __(selector) {
-    const self = {
-        element: document.querySelector(selector),
+
+import axios from 'axios';
+import { apiURL } from "../config";
+
+//date: June sixteenth Two Thuosand Twenty-Two
+export const getItem = async(id) => {
+    try {
+        const response = await axios({
+            url: `${apiURL}/api/products/${id}`,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.statusText !== 'OK') {
+            throw new error(response.data.message);
+        }
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        return { error: err.response.data.message || err.message };
 
     }
 }
+
+//function __(selector) {
+//    const self = {
+//        element: document.querySelector(selector),
+//
+//    }
+//}
--- frontend/src/screens/productPage/productSection.js
+++ frontend/src/screens/productPage/productSection.js
@@ -1,6 +1,65 @@
+import { getItem } from '../../js/kyalo';
+import { parseRequestUrl } from '../../util';
+import Rating from '../components/rating';
+
 const ProductSection = {
-    render: () => {
-        return `<div> ProductSection <\div>`;
+        render: async() => {
+                const request = parseRequestUrl();
+                const product = await getItem(request.id);
+                if (product.error) {
+                    return `<div> ${product.error}</Div>`;
+                }
+                return `
+                    <div class="item-content">
+                        <div class="back-to-main">
+                            <a href="/#/ ">home page</a>
+                        </div>
+                        <div class="item-details">
+                            <div class="details-img">
+                                <img src="${product.image}" alt="${product.name}">
+                            </div>
+                            <div class="details-info">
+                                <ul>
+                                    <li>
+                                        <h1>${product.name}</h1>
+                                    </li>
+                                    <li>
+                                    ${Rating.render({
+                                        value: product.rating,
+                                        text: `${product.numReviews} reviews`,
+                                    })}
+                                    </li>
+                                    <li>
+                                        Price: <strong>Ksh${product.price}</strong>
+                                    </li>
+                                    <li>
+                                        Description:
+                                        <div>
+                                            ${product.description}
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="details-action">
+                                    <ul>
+                                        <li>
+                                            Price: Ksh${product.price}
+                                        </li>
+                                        <li>
+                                            Status : 
+                                                ${product.countInStock > 0 
+                                                    ? `<span class="success">In  stock</span>`
+                                                    :  `<span class="denger">Unavailable</span>`
+                                                }
+                                                                        
+                                        </li>
+                                        <li>
+                                                <button id="add-button" class="fwidth primary">Add to Cart </button>
+                                        </li>
+                                    </ul>
+                            </div>
+                        </div>
+                    </div>`;
     },
 };
 export default ProductSection;
