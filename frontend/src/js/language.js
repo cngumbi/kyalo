ed7d294f72940d253322b01806c5270f9a19ef31
cngumbi <cngumbi35@gmail.com>
2022-06-28 12:35:01 +0300

    modified the languages file

--- frontend/src/js/language.js
+++ frontend/src/js/language.js
@@ -1,6 +1,7 @@
 //this main section will hold the details of the main pages
 //Author: Softcraze Corporation
 //Date Eighteenth June Two Thousand Twenty-Two
+//this is the main softcraze languages api
 
 const headerLinks = {
     login: "#",
