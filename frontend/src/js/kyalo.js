d0e2573913623998298635cd496dddb168588832
cngumbi <cngumbi35@gmail.com>
2022-06-28 18:04:02 +0300

    added the signin function to handle all signins

--- frontend/src/js/kyalo.js
+++ frontend/src/js/kyalo.js
@@ -1,5 +1,6 @@
 //creating the library
 //author: Softcraze Corporation
+//this is the main softcraze api
 
 import axios from 'axios';
 import { apiURL } from "../config";
@@ -15,12 +16,36 @@ export const getProduct = async(id) => {
             },
         });
         if (response.statusText !== 'OK') {
-            throw new error(response.data.message);
+            throw new Error(response.data.message);
         }
         return response.data;
     } catch (err) {
         console.log(err);
         return { error: err.response.data.message || err.message };
+
+    }
+};
+export const signin = async({ email, password }) => {
+    try {
+        const response = await axios({
+            url: `${apiURL}/api/users/signin`,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data: {
+                email,
+                password,
+            },
+        });
+        if (response.statusText !== 'OK') {
+            throw new error(response.data.message);
+        }
+        return response.data;
+
+    } catch (err) {
+        console.log(err);
+        return { error: err.response.data.message || err.massage };
 
     }
 }
