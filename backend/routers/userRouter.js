c234b20ef476a9edc843ecc6983a8a4233e8e9d9
cngumbi <cngumbi35@gmail.com>
2022-06-28 18:07:36 +0300

    added the UserRouter.post to enable the post request to be send

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import User from '../models/userModel';
+import expressAsyncHandler from 'express-async-handler';
+import { generateToken } from '../util';
 
 const UserRouter = express.Router();
 
-UserRouter.get('/createadmin', async(req, res) => {
+UserRouter.get('/createadmin', expressAsyncHandler(async(req, res) => {
     try {
         const user = new User({
             name: 'admin',
@@ -16,5 +18,24 @@ UserRouter.get('/createadmin', async(req, res) => {
     } catch (err) {
         res.status(500).send({ message: err.message });
     }
-});
+}));
+UserRouter.post('/signin', expressAsyncHandler(async(req, res) => {
+    const signinUser = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    });
+    if (!signinUser) {
+        res.status(401).send({
+            message: 'Invalid Email or Password',
+        });
+    } else {
+        res.send({
+            _id: signinUser._id,
+            name: signinUser.name,
+            email: signinUser.email,
+            isAdmin: signinUser.isAdmin,
+            token: generateToken(signinUser),
+        });
+    }
+}));
 export default UserRouter;
