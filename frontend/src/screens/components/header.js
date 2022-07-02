08c812cb27df77f88c397a35cae4b8f02fb57830
cngumbi <cngumbi35@gmail.com>
2022-07-02 15:21:58 +0300

    change the header file to dispaly login port without the chat sectin

--- frontend/src/screens/components/header.js
+++ frontend/src/screens/components/header.js
@@ -5,20 +5,20 @@ const Header = {
         render: () => {
                 const { name } = getUserInfo();
                 return `
-            <div class="brand">
-                <a href="#">kwi<span>t&#361;</span></a>
-            </div>
-            <div class="list-items">
-            <a href="#/cart">chat</a>
-            ${
-                name
-                    ? `<a href="#/profile">${name}</a>` 
-                    : `<a href="#/signin">login</a>`
-            }
-            </div>
-        `;
-    },
-    after_render: () => {},
+                    <div class="brand">
+                        <a href="#">kwi<span>t&#361;</span></a>
+                    </div>
+                    <div class="list-items">
+                    ${
+                        name
+                            ? `<a href="#/profile">${name}</a>
+                                <a href="#/cart">chat</a>` 
+                            : `<a href="#/signin">login</a>`
+                    }
+                    </div>
+                `;
+        },
+        after_render: () => {},
 
 };
 export default Header;
