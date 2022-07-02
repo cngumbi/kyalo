b93cb611a85bf5f32bcb19397668b6f935c52f90
cngumbi <cngumbi35@gmail.com>
2022-07-02 15:23:30 +0300

    created update and register functions

--- frontend/src/js/kyalo.js
+++ frontend/src/js/kyalo.js
@@ -4,6 +4,7 @@
 
 import axios from 'axios';
 import { apiURL } from "../config";
+import { getUserInfo } from '../localStorage';
 
 //date: June sixteenth Two Thuosand Twenty-Two
 export const getProduct = async(id) => {
@@ -48,7 +49,60 @@ export const signin = async({ email, password }) => {
         return { error: err.response.data.message || err.massage };
 
     }
-}
+};
+export const register = async({ name, email, password }) => {
+    try {
+        const response = await axios({
+            url: `${apiURL}/api/users/register`,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data: {
+                name,
+                email,
+                password,
+            },
+        });
+        if (response.statusText !== 'OK') {
+            throw new error(response.data.message);
+        }
+        return response.data;
+
+    } catch (err) {
+        console.log(err);
+        return { error: err.response.data.message || err.massage };
+
+    }
+};
+//update function
+export const update = async({ name, email, password }) => {
+    try {
+        const { _id, token } = getUserInfo();
+        const response = await axios({
+            url: `${apiURL}/api/users/${_id}`,
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `bearer ${token}`,
+            },
+            data: {
+                name,
+                email,
+                password,
+            },
+        });
+        if (response.statusText !== 'OK') {
+            throw new error(response.data.message);
+        }
+        return response.data;
+
+    } catch (err) {
+        console.log(err);
+        return { error: err.response.data.message || err.massage };
+
+    }
+};
 
 //function __(selector) {
 //    const self = {
