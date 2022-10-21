6c6ff618c45240d821606152f1437269393d76c3
cngumbi <cngumbi35@gmail.com>
2022-10-21 05:00:59 +0300

    finished the main-page design by making it simple and presintable

--- frontend/src/screens/homePage/mainSection.js
+++ frontend/src/screens/homePage/mainSection.js
@@ -1,21 +1,34 @@
-//setting the user on the main page
-//import { signin } from '../../js/kyalo';
-//import { getUserInfo, setUserInfo } from '../../localStorage';
-//import { hideLoading, redirectUser, showLoading, showMessage } from '../../util';
-//import data from "../data.js";
-//import axios from 'axios';
-//import { getProducts } from '../../js/kyalo';
-//import { hideLoading, showLoading } from '../../util';
-//import Rating from '../components/rating'
-//import SigninSection from "../signups/signinSection";
+import { signin } from "../../js/kyalo";
+import { getUserInfo, setUserInfo } from "../../localStorage";
+import { hideLoading, redirectUser, showLooding, showMessage } from "../../util";
+
 const MainSection = {
     after_render: () => {
-
+        document
+            .getElementById('signin-form')
+            .addEventListener('submit', async(e) => {
+                e.preventDefault();
+                showLooding();
+                const data = await signin({
+                    email: document.getElementById('email').value,
+                    password: document.getElementById('password').value
+                });
+                hideLoading();
+                if (data.error) {
+                    showMessage(data.error);
+                } else {
+                    setUserInfo(data);
+                    redirectUser();
+                }
+            });
     },
     render: async() => {
+        if (getUserInfo().name) {
+            redirectUser();
+        }
         return `
             <div class="main-page">
-                <div class="home-header bottom-margin">
+               <!-- <div class="home-header bottom-margin">
                     <ul>
                         <li class="space-between"><a>tournaments</a></li>
                         <li class="space-between"><a>new games</a></li>
@@ -23,35 +36,41 @@ const MainSection = {
                         <li class="space-between"><a>match fixture</a></li>
                         <li class="space-between"><a>league</a></li>
                     </ul>
-                </div> 
+                </div> -->
                 <div class="row">
-                    <div class="col-1-of-2">
+                    <div class="col-1-of-2 top-margin">
                         <div class="motor">
                             <h1>how teams </h1>
-                            <h1>should play</h1>
+                            <h1>play</h1>
                         </div>
                         <div class="motivation bottom-margin">
-                            <h2>Forget the old rules. You can Play the Best teams</h2>
+                            <h2>Forget the old rules. You can Play the Best teams.</h2>
                             <h2>Right here. Right now</h2>
                         </div>
-                        <div class="row">
-                            <div class="col-1-of-2"><div class="button-fill button-cursor button-text"><a href="/#/register">Join Us</a></div></div>
-                            <div class="col-1-of-2"><div class="button-clear button-cursor button-text"><a href="/#/signin">Sign In</a></div></div>
+                    </div>
+                    <div class="col-1-of-2 top-margin bottom-margin">
+                        <div class="form-container">
+                            <form id="signin-form">
+                                <ul class="form-items">
+                                    <li>
+                                        <h1>SIGN IN</h1>
+                                    </li>
+                                    <li>
+                                        <label for="email">Email</label>
+                                        <input type="email" name="email" id="email"/>
+                                    </li>
+                                    <li>
+                                        <label for="password">Password</label>
+                                        <input type="password" name="password" id="password"/>
+                                    </li>
+                                    <li><button type="submit" class="button-fill">SIGN IN</button></li>
+                                    <li><div>NEW USER?<a href="/#/register">Create an account with us</a></div></li>
+                                </ul>
+                            </form>
                         </div>
-                        <div >
-                            <h6>sponsored by</h6>
-                            <ul class="composition">
-                                <li><img src="" alt="" class="">logo image</li>
-                                <li><img src="" alt="" class="">logo image</li>
-                                <li><img src="" alt="" class="">logo image</li>
-                            </ul>
-                        </div>                       
-                    </div>
-                    <div class="col-1-of-2">
-                    </div>
-                    
+                    </div> 
                 </div>
-            </div>   
+            </div>            
         `;
     },
 };
