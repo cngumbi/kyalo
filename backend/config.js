85af3ca54da0c189a7b80fd1303a29246e23f695
cngumbi <cngumbi35@gmail.com>
2022-10-30 09:31:41 +0300

    Revert "made new changas"
    This reverts commit fd778ebb60208941efd1b176fae2aeb502c3a7b9.

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,12 @@
-import dotenv from 'dotenv';
+//this the configuration file of the whole project
+//it includes:
+//      1: data base configuration
+//      2: payment configuration
+
+import dotenv from 'dotenv'; //also you can use import 'dotenv/config'
 
 dotenv.config();
+//console.log(process.env.MONGODB_URL)
 export default {
     MONGODB_URL: process.env.MONGODB_URL,
     JWT_SECRET: process.env.JWT_SECRET,
