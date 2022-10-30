13bdade2bbe8bca349536ed0b58b1418646900ab
cngumbi <cngumbi35@gmail.com>
2022-10-30 09:06:35 +0300

    made new changas

--- backend/config.js
+++ backend/config.js
@@ -1,12 +1,6 @@
-//this the configuration file of the whole project
-//it includes:
-//      1: data base configuration
-//      2: payment configuration
-
-import dotenv from 'dotenv'; //also you can use import 'dotenv/config'
+import dotenv from 'dotenv';
 
 dotenv.config();
-//console.log(process.env.MONGODB_URL)
 export default {
     MONGODB_URL: process.env.MONGODB_URL,
     JWT_SECRET: process.env.JWT_SECRET,
