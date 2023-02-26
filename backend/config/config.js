facf47cbf36a187466f1ac17e8c73c0a50f14427
cngumbi <cngumbi35@gmail.com>
2023-02-26 14:15:53 +0300

    movied the configaration file to a single directorate

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,4 +5,5 @@ export default {
     MONGODB_URL: process.env.MONGODB_URL,
     JWT_SECRET: process.env.JWT_SECRET,
     PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
+    PORT: process.env.PORT || 3000,
 };
