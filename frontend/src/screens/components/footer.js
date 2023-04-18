f6ae162c0fe5ddcb13456130f33004c5dff47d20
cngumbi <cngumbi35@gmail.com>
2023-04-18 12:35:56 +0300

    added footer

--- frontend/src/screens/components/footer.js
+++ frontend/src/screens/components/footer.js
@@ -1,7 +1,25 @@
+import { getUserInfo } from "../../localStorage";
+
 const Footer = {
     render: () => {
+        const { name } = getUserInfo();
         return `
-            <div>all rights reserved @ 2022 softcraze Corporation</div>
+        ${name ? `
+            <div>
+                <div class="sidebar-useful-link">
+                    <a href="#">About</a>
+                    <a href="#">Accessibility</a>
+                    <a href="#">Help Center</a>
+                    <a href="#">Privacy Policy</a>
+                    <a href="#">Advertising</a>
+                    <a href="#">Get the App</a>
+                    <a href="#">More</a>
+                    <div class="copyright-msg">
+                        <p>Softcraze Corporation &#169; 2023. all rights reserved</p>
+                    </div>
+                </div>
+            </div>`
+        : ''}
         `;
 
     },
