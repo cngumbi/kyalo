fcd2f720e8edc756724b654800d078b8c14a3ae1
cngumbi <cngumbi35@gmail.com>
2023-04-18 17:54:53 +0300

    added new file

---
+++ frontend/src/screens/main/components/educationContent.js
@@ -0,0 +1,19 @@
+const EducationSection = {
+    render: ()=>{
+        return `
+        <div class="profile-description">
+            <h2>Education</h2>
+            <div class="profile-desc-row">
+                <img src="../images/soccer.png">
+                <div>
+                    <h3>Embu University</h3>
+                    <b> BSC, Computer Science</b>
+                    <b>2018 - 2022</b>
+                    <hr>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+}
+ export default EducationSection;
