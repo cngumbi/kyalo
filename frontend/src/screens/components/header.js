128b7711ffe8012b3f2e355fff1dd353e653ba26
cngumbi <cngumbi35@gmail.com>
2023-04-22 20:23:13 +0300

    corrected errors

--- frontend/src/screens/components/header.js
+++ frontend/src/screens/components/header.js
@@ -1,54 +1,57 @@
-import { getUserInfo } from "../../localStorage";
+import { getUserInfo } from '../../localStorage';
 
 //this section handles the header of the pages
 const Header = {
-        after_render: () => {
-            //document.getElementById("signout").addEventListener("click", () => {
-            //    clearUser();
-            //    document.location.hash = '/';
-            //})
-            
-            //function toggleMenu(){
-            //    const profileMenu = document.querySelector(".profile-menu-wrap");
-            //    profileMenu.classList.toggle("open-menu");
-            //}
-            window.addEventListener("scroll", function(){
-                var header = document.querySelector("header");
-                header.classList.toggle("sticky", window.scrollY > 0);
-            });
-            window.addEventListener("click", ()=>{
-                let profileMenu = document.getElementById("profileMenu");
-                profileMenu.classList.toggle("open-menu");
-            });
-            //let profileDropdown = document.getElementById("profileMenu");
-            //let btn = document.querySelector(".nav-profile-img");
-            //const toggleMenu = ()=> profileDropdown.classList.toggle("open-menu");
-            //window.addEventListener('click', function(e){
-            //    if(!btn.contains(e.target)){
-            //        profileDropdown.classList.remove('open-menu');
-            //    }
-            //})
-        },
-        render: () => {
-                const { name, isAdmin } = getUserInfo();
-                return `
+  after_render: () => {
+    //document.getElementById('signout').addEventListener('click', () => {
+    //  clearUser();
+    //  document.location.hash = '/';
+    //});
+    window.addEventListener('scroll', function () {
+      var header = document.querySelector('header');
+      header.classList.toggle('sticky', window.scrollY > 0);
+    });
+    document.getElementById('dropDown').addEventListener('click', () => {
+      const profileMenu = document.querySelector('.profile-menu-wrap');
+      profileMenu.classList.toggle('open-menu');
+      let btn = document.querySelector('.nav-profile-img');
+      window.addEventListener('click', function (q) {
+        if (!btn.contains(q.target)) {
+          profileMenu.classList.remove('open-menu');
+        }
+      });
+    });
+  },
+  render: () => {
+    const { name, isAdmin } = getUserInfo();
+    return `
                     <header>
                         <nav class="navbar">
                             <div class="navbar-left">
                                 <div class="brand">
-                                    ${ name ? `
+                                    ${
+                                      name
+                                        ? `
                                         <a href="/#/kwitumain" class="logo">kwit&#361;</a>
                                         
-                                        `: `<a href="#" class="logo">kwit&#361</a>`}
+                                        `
+                                        : `<a href="#" class="logo">kwit&#361</a>`
+                                    }
                                 </div>
-                                ${name ? `
+                                ${
+                                  name
+                                    ? `
                                     <div class="search-box">
                                         <img src="" alt="">
                                         <input type="text" placeholder="Search" id="search">
                                     </div>
-                                `:``}
+                                `
+                                    : ``
+                                }
                             </div>
-                            ${name ? `
+                            ${
+                              name
+                                ? `
                             <div class="navbar-center">
                                 <ul>
                                     <li><span><a href="/#/profile">${name}</a></span></li>
@@ -56,7 +59,7 @@ const Header = {
                             </div>
                             <div class="navbar-right">
                                 <div class="online">
-                                    <img src="../../images/yy.jpg" alt="" class="nav-profile-img">
+                                    <button type="button" id="dropDown"><img src="../../images/yy.jpg" alt="" class="nav-profile-img"></button>
                                 </div>
                                 <!--profile dropdown menu-->
                                 <div class="profile-menu-wrap" id="profileMenu">
@@ -79,19 +82,21 @@ const Header = {
                                             <p>Settings</p>
                                             <span>></span>
                                         </a>
-                                        <!--<a href="" class="profile-menu-link" id="signout">
+                                        <!--<a href="" class="profile-menu-link">
                                             <img src="soccer.png" alt="">
-                                            <p>logout</p>
+                                            <button type="button" id="signout"><p>logout</p></button>
                                             <span>></span>
                                         </a>-->
                                     </div>
                                 </div>
                             </div>
-                            `:''}
+                            `
+                                : ''
+                            }
                         </nav>                       
                     </header>
                    
                 `;
-        }
+  },
 };
 export default Header;
