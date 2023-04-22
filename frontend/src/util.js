999ce0ea1591e1745d970158cdf84fa4a33f3b66
cngumbi <cngumbi35@gmail.com>
2023-04-22 19:02:15 +0300

    corrected errors

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,65 +1,66 @@
 // create a function parseRequesturl to handle the urls
 //Author: Softcraze Corporation
 //Date Eighteenth June Two Thousand Twenty-Two
-import { getUserInfo } from "./localStorage";
+import { getUserInfo } from './localStorage';
 
 export const parseRequestUrl = () => {
-    const url = document.location.hash.toLowerCase();
-    const request = url.split("/");
-    return {
-        resource: request[1],
-        id: request[2],
-        action: request[3],
-    };
+  const url = document.location.hash.toLowerCase();
+  const request = url.split('/');
+  return {
+    resource: request[1],
+    id: request[2],
+    verb: request[3],
+  };
 };
-export const rerender = async(component) => {
-    document.getElementById('main-container').innerHTML = await component.render();
-    await component.after_render();
+export const rerender = async (component) => {
+  document.getElementById('main-container').innerHTML =
+    await component.render();
+  await component.after_render();
 };
 export const showLoading = () => {
-    document.getElementById('loading-overlay').classList.add('active');
+  document.getElementById('loading-overlay').classList.add('active');
 };
 export const hideLoading = () => {
-    document.getElementById('loading-overlay').classList.remove('active');
+  document.getElementById('loading-overlay').classList.remove('active');
 };
 //--------------------------------------------------------------------------
 //validation section
 //--------------------------------------------------------------------------
 export const showMessage = (message, callback) => {
-    // Get the message overlay element
-    const messageOverlay = document.getElementById('message-overlay');
-  
-    // Create the message content
-    const messageContent = `
+  // Get the message overlay element
+  const messageOverlay = document.getElementById('message-overlay');
+
+  // Create the message content
+  const messageContent = `
     <div>
       <div id="message-content">${message}</div>
       <button id="close-button">OK</button>
     </div>
     `;
-  
-    // Set the message overlay content and show it
-    messageOverlay.innerHTML = messageContent;
-    messageOverlay.classList.add('active');
-  
-    // Add a click listener to the OK button
-    const okButton = document.getElementById('close-button');
-    okButton.addEventListener('click', () => {
-      // Hide the message overlay
-      messageOverlay.classList.remove('active');
-      
-      // Call the callback function if provided
-      if (callback) {
-        callback();
-      }
-    });
+
+  // Set the message overlay content and show it
+  messageOverlay.innerHTML = messageContent;
+  messageOverlay.classList.add('active');
+
+  // Add a click listener to the OK button
+  const okButton = document.getElementById('close-button');
+  okButton.addEventListener('click', () => {
+    // Hide the message overlay
+    messageOverlay.classList.remove('active');
+
+    // Call the callback function if provided
+    if (callback) {
+      callback();
+    }
+  });
 };
 export const redirectUser = () => {
-  if(!getUserInfo().name){
+  if (!getUserInfo().name) {
     window.location.assign('/');
-  }else{
+  } else {
     window.location.assign('/#/kwitumain');
   }
-}
+};
 //------------------------------------------------------------------------------
 //export const redirectUser = () => {
 //    const redirectTo = getCartItems().length !== 0 ? '/shipping' : getUserInfo().name ? '/dashboard' : '/';
